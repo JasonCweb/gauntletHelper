00d2e4d8f29e8bc491ed60db50cc989e1649d758
Jason Choe <jasonkmchoe@gmail.com>
2018-04-14 15:47:37 -0400

    attempt to add name to card

--- gauntletHelper.js
+++ gauntletHelper.js
@@ -287,7 +287,7 @@ var GauntletHelperModule = function () {
 
                         if (name in rankings) {
                             rankings[name] = {
-                                ranking: rankings[name].ranking + ' - ' + match[1] + ': ' + match[3],
+                                ranking: name + rankings[name].ranking + ' - ' + match[1] + ': ' + match[3],
                                 description: rankings[name].description + ' ' + match[1] + ': ' + match[4]
                             };
                         } else {
