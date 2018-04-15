31596b836631046fb1345d76f0dade3a0885d16e
Jason Choe <jasonkmchoe@gmail.com>
2018-04-15 17:59:35 -0400

    shifting left, shrinking

--- gauntletHelper.js
+++ gauntletHelper.js
@@ -37,7 +37,7 @@ var GauntletHelperModule = function () {
 
     var createDivs = function() {
         if (!divsCreated && $(parentDiv).length) {
-            var width = 450;
+            var width = 400;
             var height = 205;
             var marginBottom = 200;
             var separation = 6;
@@ -64,7 +64,7 @@ var GauntletHelperModule = function () {
                 width: generalDivWidth,
                 height: 'auto',
                 'margin-bottom': marginBottom + height + separation,
-                'margin-left': (-generalDivWidth / 2 - 250)
+                'margin-left': (-generalDivWidth / 2 - 290)
             }).appendTo($(parentDiv).css('position', 'relative'));
 
             var textStyle = {
@@ -77,13 +77,13 @@ var GauntletHelperModule = function () {
             cardDivs = [3];
 
             // Card IDs are backwards, so these are backwards to fix it
-            cardDivs[2] = $('<div />').css(style).css(cardStyle).css({ 'margin-left': (-width / 2) - (width + separation) - 250 })
+            cardDivs[2] = $('<div />').css(style).css(cardStyle).css({ 'margin-left': (-width / 2) - (width + separation) - 290 })
                 .appendTo($(parentDiv).css('position', 'relative'));
 
-            cardDivs[1] = $('<div />').css(style).css(cardStyle).css({ 'margin-left': (-width / 2) - 250 })
+            cardDivs[1] = $('<div />').css(style).css(cardStyle).css({ 'margin-left': (-width / 2) - 290 })
                 .appendTo($(parentDiv).css('position', 'relative'));
 
-            cardDivs[0] = $('<div />').css(style).css(cardStyle).css({ 'margin-left': (-width / 2) + (width + separation) - 250 })
+            cardDivs[0] = $('<div />').css(style).css(cardStyle).css({ 'margin-left': (-width / 2) + (width + separation) - 290 })
                 .appendTo($(parentDiv).css('position', 'relative'));
 
             // Setting visibility to hidden in the CSS seems to make it always hidden, so doing this instead
