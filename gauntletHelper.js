e4b77dfb1d44c490a55ed30f7c603915b4e985aa
Jason Choe <jasonkmchoe@gmail.com>
2018-04-15 19:38:50 -0400

    adjusted background/main text colors

--- gauntletHelper.js
+++ gauntletHelper.js
@@ -46,7 +46,7 @@ var GauntletHelperModule = function () {
                 position: 'absolute',
                 bottom: '50%',
                 left: '50%',
-                //backgroundColor: '#1E1E1E',
+                backgroundColor: '#1E1E1E',
                 padding: 6,
                 'overflow-y': 'hidden',
                 'border-radius': 8,
@@ -99,9 +99,9 @@ var GauntletHelperModule = function () {
     var updateCard = function(i) {
         var name = getCardName(currentCards[i]);
         if (name in rankings) {
-            cardDivs[i].html('<p align=center style="font-size:.7em;line-height:1em;color:orange">' + name + '</p><p align=center style="font-size:1em;line-height:1em;color:khaki">' + rankings[name].ranking + '</p><p style="font-size:0.7em;line-height:1em;color:#EEE8AA">' + rankings[name].description + '</p>');
+            cardDivs[i].html('<p align=center style="font-size:.7em;line-height:1em;color:orange">' + name + '</p><p align=center style="font-size:1em;line-height:1em;color:khaki">' + rankings[name].ranking + '</p><p style="font-size:0.7em;line-height:1em;color:#9cdcdb">' + rankings[name].description + '</p>');
         } else{
-            cardDivs[i].html('<p align=center style="font-size:1em;line-height:1em;color:#EEE8AA">\'' + name + '\' not found in rankings.</p>');
+            cardDivs[i].html('<p align=center style="font-size:1em;line-height:1em;color:#9cdcdb">\'' + name + '\' not found in rankings.</p>');
         }
     }
 
@@ -171,7 +171,7 @@ var GauntletHelperModule = function () {
                 result += '</ul></td>';
             }
             if (i == 0 || i == splitIndex) {
-                result += '<td width="50%" valign="top"><ul style="font-size:0.7em;line-height:1em;color:#EEE8AA;list-style-type:disc;padding-left:1.5em">';
+                result += '<td width="50%" valign="top"><ul style="font-size:0.7em;line-height:1em;color:#9cdcdb;list-style-type:disc;padding-left:1.5em">';
             }
 
             result += '<li>' + notes[i] + '</li>';
